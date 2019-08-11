d726fae31d5aff7e040f9fb7d5da80dd549c6bcb
Dmitry <nikonorovds@yandex.ru>
2019-08-11 17:34:53 +0300

    Задание №3: Подключить тестовый фреймворк и сделать первые тесты

---
+++ sandbox/src/test/java/ru/stqa/d7/sandbox/PointTestsPositive.java
@@ -0,0 +1,14 @@
+package ru.stqa.d7.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTestsPositive {
+
+  @Test
+  public void testPoint(){
+    Point p = new Point(1,5,2,3);
+    Assert.assertEquals(p.distance(), 2.23606797749979);
+  }
+
+}
