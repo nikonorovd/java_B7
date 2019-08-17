a7a24bc4926f83ebc6034a4562732497e61825b2
Dmitry <nikonorovds@yandex.ru>
2019-08-17 20:31:26 +0300

    Создан класс по удалению контакта. Удаление и создание контакта запускаются.

---
+++ addressbook-web-tests/src/test/java/ru/stqa/d7/addressbook/ContactDelitionTests.java
@@ -0,0 +1,57 @@
+package ru.stqa.d7.addressbook;
+
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ContactDelitionTests {
+  private WebDriver wd;
+
+
+  @BeforeMethod(alwaysRun = true)
+  public void setUp() throws Exception {
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testContactDelition() throws Exception {
+    wd.get("http://localhost/");
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.id("LoginForm")).click();
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.xpath("//html")).click();
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
+    wd.findElement(By.id("6")).click();
+    wd.findElement(By.xpath("//input[@value='Delete']")).click();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void tearDown() throws Exception {
+    wd.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+}
