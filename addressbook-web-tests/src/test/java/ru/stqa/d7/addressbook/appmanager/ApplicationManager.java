5a8b001688cf76d598cee71c04324e162794e38c
Dmitry <nikonorovds@yandex.ru>
2019-08-18 20:13:48 +0300

    Лекция 2.8. Создаём общий базовый класс для классов-помощников (тесты запускаются, необходимо преобразовать класс ContactHelper)

--- addressbook-web-tests/src/test/java/ru/stqa/d7/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/d7/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,6 @@
 package ru.stqa.d7.addressbook.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -32,23 +31,6 @@ public class ApplicationManager {
     wd.quit();
   }
 
-  public boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  public boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
 
   public GroupHelper getGroupHelper() {
     return groupHelper;
@@ -60,5 +42,14 @@ public class ApplicationManager {
 
   public ContactHelper getContactHelper() {
     return contactHelper;
+  }
+
+  public boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/d7/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/d7/addressbook/appmanager/NavigationHelper.java
@@ -3,18 +3,18 @@ package ru.stqa.d7.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper {
-  private WebDriver wd;
+public class NavigationHelper extends HelperBase{
 
   public NavigationHelper(WebDriver wd) {
-    this.wd = wd;
+    super(wd);
+
   }
 
   public void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
+   click(By.linkText("groups"));
   }
 
   public void gotoContactPage() {
-    wd.findElement(By.linkText("add new")).click();
+    click(By.linkText("add new"));
   }
 }
