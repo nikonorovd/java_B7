2419a20b29437fdae2635bbadc398ad0b5df09d8
Dmitry <nikonorovds@yandex.ru>
2019-08-20 20:39:42 +0300

    Лекция 3.3. Запуск тестов в разных браузерах (тесты запускаются)

--- addressbook-web-tests/src/test/java/ru/stqa/d7/addressbook/Tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/d7/addressbook/Tests/TestBase.java
@@ -3,10 +3,13 @@ package ru.stqa.d7.addressbook.Tests;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.d7.addressbook.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+
+
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
--- addressbook-web-tests/src/test/java/ru/stqa/d7/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/d7/addressbook/appmanager/ApplicationManager.java
@@ -3,9 +3,14 @@ package ru.stqa.d7.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
 
 import java.util.concurrent.TimeUnit;
+
 
 public class ApplicationManager {
   public WebDriver wd;
@@ -14,9 +19,21 @@ public class ApplicationManager {
   private SessionHelper sessionHelper;
   private  NavigationHelper navigationHelper;
   private  GroupHelper groupHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new FirefoxDriver();
+    if (browser == BrowserType.FIREFOX){
+      wd = new FirefoxDriver();
+    }else if (browser == BrowserType.CHROME){
+      wd = new ChromeDriver();
+    }else if (browser == BrowserType.IE){
+      wd = new InternetExplorerDriver();
+    }
+
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/");
     groupHelper = new GroupHelper(wd);
