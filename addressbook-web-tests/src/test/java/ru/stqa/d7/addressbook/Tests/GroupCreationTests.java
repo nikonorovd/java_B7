49e47d5840bcab9999ac61bb1987c5fc33be1400
Dmitry <nikonorovds@yandex.ru>
2019-08-25 21:53:31 +0300

    Лекция 4.5. Формирование коллекции (тесты для групп запускаются)

--- addressbook-web-tests/src/test/java/ru/stqa/d7/addressbook/Tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/d7/addressbook/Tests/GroupCreationTests.java
@@ -4,15 +4,17 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.d7.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupCreationTests extends TestBase{
 
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
-    int before = app.getGroupHelper().getGroupCout();
+    List<GroupData>before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().createGroup(new GroupData("Test1", null, null));
-    int after = app.getGroupHelper().getGroupCout();
-    Assert.assertEquals(after, before +1);
+    List<GroupData>after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() +1);
 
 
   }
--- addressbook-web-tests/src/test/java/ru/stqa/d7/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/d7/addressbook/appmanager/GroupHelper.java
@@ -3,7 +3,11 @@ package ru.stqa.d7.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import ru.stqa.d7.addressbook.model.GroupData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupHelper extends HelperBase {
 
@@ -70,5 +74,16 @@ public class GroupHelper extends HelperBase {
 
   public int getGroupCout() {
     return wd.findElements(By.name("selected[]")).size();
+  }
+
+  public List<GroupData> getGroupList() {
+    List<GroupData> groups = new ArrayList<>();
+    List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+    for(WebElement element : elements){
+      String name = element.getText();
+      GroupData group = new GroupData(name, null, null);
+      groups.add(group);
+    }
+    return groups;
   }
 }
