95299bd8d6415235f09019a75219b635f60f986e
Dmitry <nikonorovds@yandex.ru>
2019-10-01 18:11:39 +0300

    Лекция 5.1. Как правильно запускать браузер (+)

--- addressbook-web-tests/src/test/java/ru/stqa/d7/addressbook/Tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/d7/addressbook/Tests/ContactCreationTests.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class ContactCreationTests extends TestBase{
 
 
-  @Test
+  @Test (enabled = false)
   public void testContactCreation()  {
     List<ContactData> before = app.getContactHelper().getContactList();
     app.getNavigationHelper().gotoContactPage();
