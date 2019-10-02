5db4cc856635321a766fcb79f87d5541ff4c38d2
Dmitry <nikonorovds@yandex.ru>
2019-10-02 14:31:01 +0300

    Лекция 5.4. Fluent-интерфейсы (+)

--- addressbook-web-tests/src/test/java/ru/stqa/d7/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/d7/addressbook/model/GroupData.java
@@ -1,32 +1,37 @@
 package ru.stqa.d7.addressbook.model;
 
 public class GroupData {
-  private int id;
-  private final String name;
-  private final String header;
-  private final String footer;
+  private int id = Integer.MAX_VALUE;;
+  private String name;
+  private String header;
+  private String footer;
 
 
   public int getId() {
     return id;
   }
 
-  public void setId(int id) {
-    this.id = id;
+  public GroupData withName(String name) {
+    this.name = name;
+    return this;
+
   }
 
-  public GroupData(String name, String header, String footer) {
-    this.id = Integer.MAX_VALUE;
-    this.name = name;
+  public GroupData withHeader(String header) {
     this.header = header;
-    this.footer = footer;
+    return this;
+
   }
 
-  public GroupData(int id, String name, String header, String footer) {
-    this.id = id;
-    this.name = name;
-    this.header = header;
+  public GroupData withFooter(String footer) {
     this.footer = footer;
+    return this;
+
+  }
+
+  public GroupData withId(int id) {
+    this.id = id;
+    return this;
   }
 
   public String getName() {
@@ -66,6 +71,5 @@ public class GroupData {
   public int hashCode() {
     return name != null ? name.hashCode() : 0;
   }
-
 
 }
